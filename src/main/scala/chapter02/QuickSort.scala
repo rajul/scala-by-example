ea0c1902504b8d8218590afcefcb6446414b4d4d
Rajul <rajul09@gmail.com>
2018-10-26 09:24:44 +0800

    Chapter 2: Quicksort

---
+++ src/main/scala/chapter02/QuickSort.scala
@@ -0,0 +1,55 @@
+package chapter02
+
+class QuickSort {
+  def sortProcedural(xs: Array[Int]) = {
+    def swap(i: Int, j: Int) = {
+      val t = xs(i)
+      xs(i) = xs(j)
+      xs(j) = t
+    }
+
+    def sort1(l: Int, r: Int): Unit = {
+      println(xs.toList)
+
+      val pivot = xs((l + r) / 2)
+      var i = l
+      var j = r
+
+      while (i <= j) {
+        while (xs(i) < pivot)
+          i += 1
+
+        while (xs(j) > pivot)
+          j -= 1
+
+        if(i <= j) {
+          swap(i, j)
+          i += 1
+          j -= 1
+        }
+
+        if (l < j)
+          sort1(l, j)
+
+        if (j < r)
+          sort1(i, r)
+      }
+    }
+
+    sort1(0, xs.length - 1)
+  }
+
+
+  def sortFunctional(xs: Array[Int]): Array[Int] = {
+    if (xs.length <= 1)
+      xs
+    else {
+      val pivot = xs(xs.length / 2)
+      Array.concat(
+        sortFunctional(xs filter(pivot >)),
+        xs filter(pivot ==),
+        sortFunctional(xs filter(pivot <))
+      )
+    }
+  }
+}
